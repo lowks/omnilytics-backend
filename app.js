a5939da34d2f9b94645a50f3d9aad9b6838b0c7a
Muhammad Lukman Low <lowkster.etiqa@gmail.com>
2021-02-23 18:03:27 +0800

    Challenge solution initial

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const router = require("./routes/routes")();
+
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", router);
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
+
+module.exports = app;
